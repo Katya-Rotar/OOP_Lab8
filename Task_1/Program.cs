4ff9f6c8cc838a932b31c290260c07746d26453c
user <katya.rotar02@gmail.com>
2023-10-15 22:47:23 +0300

    Task_1, Problem 0 Problem 1 Problem 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string input_string = Console.ReadLine();
+            int input_int;
+            if (int.TryParse(input_string, out input_int))
+            {
+                Box<int> intBox = new Box<int>(input_int);
+                Console.WriteLine(intBox.ToString());
+            }
+            else
+            {
+                Box<string> stringBox = new Box<string>(input_string);
+                Console.WriteLine(stringBox.ToString());
+            }
+        }
+    }
+}
