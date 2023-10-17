85ebd0bd25065246607b3ebe9400de3332e395d5
user <katya.rotar02@gmail.com>
2023-10-17 11:10:10 +0300

    Task_3

---
+++ Task_3.Problem_5, Problem_6/Box.cs
@@ -0,0 +1,15 @@
+﻿class Box <T>{
+    public T value { set; get; }
+    private int n = 0;
+    public Box(T value) {
+        this.value = value;
+    }
+    public int ComparingElements(List<Box<T>> list, T value) {
+        foreach (Box<T> item in list) {
+            if (Comparer<T>.Default.Compare(item.value, value) > 0) {
+                n++;
+            }
+        }
+        return n;
+    }
+}
---
+++ Task_3.Problem_5, Problem_6/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<Box<string>> list_string = new List<Box<string>>();
+        List<Box<double>> list_double = new List<Box<double>>();
+
+        Box<double> doubleBox = null;
+        Box<string> box = null;
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            double box_double;
+            if (double.TryParse(input, out box_double))
+            {
+                doubleBox = new Box<double>(box_double);
+                list_double.Add(doubleBox);
+            }
+            else
+            {
+               box = new Box<string>(input);
+               list_string.Add(box);
+            }
+        }
+        string input_text= Console.ReadLine();
+        int sumElement = 0;
+        if (double.TryParse(input_text, out double intList))
+        { 
+            sumElement = doubleBox.ComparingElements(list_double, intList);
+        }
+        else {
+            sumElement = box.ComparingElements(list_string, input_text);
+        }
+        Console.WriteLine(sumElement);
+    }
+}
